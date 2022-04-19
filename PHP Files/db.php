d7aebce3cf5a26f5a5e65131a153220a5c15a85a
Yılmaz Yurdakul <35039656+yilmazyurdakul@users.noreply.github.com>
2022-04-19 21:28:57 +0300

    Update db.php
    Hiding credentals...

--- PHP Files/db.php
+++ PHP Files/db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "yilmazyu_admin";
-$password = "147896";
-$dbname = "yilmazyu_proje";
+$username = "****";
+$password = "****";
+$dbname = "****";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
